165168acc60b47ab6a5cf0eecdab02ea45e96c67
Amin Mohammed-Coleman <amin@digitalpatterns.io>
2020-06-22 14:50:12 +0100

    adding test

--- src/test/groovy/io/digital/patterns/workflow/models/bpmn/SendNotificationBpmnSpec.groovy
+++ src/test/groovy/io/digital/patterns/workflow/models/bpmn/SendNotificationBpmnSpec.groovy
@@ -2,10 +2,14 @@ package io.digital.patterns.workflow.models.bpmn
 
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder
 import com.amazonaws.services.simpleemail.model.VerifyEmailIdentityRequest
 import com.amazonaws.services.sns.AmazonSNSClient
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+import com.github.tomjankes.wiremock.WireMockGroovy
 import io.digital.patterns.workflow.notification.AmazonSMSService
 import io.digital.patterns.workflow.pdf.PdfService
 import org.camunda.bpm.engine.runtime.ProcessInstance
@@ -18,25 +22,38 @@ import org.junit.Assert
 import org.junit.ClassRule
 import org.springframework.core.env.Environment
 import org.springframework.core.env.StandardEnvironment
+import org.springframework.core.io.ClassPathResource
 import org.springframework.web.client.RestTemplate
 import org.testcontainers.containers.localstack.LocalStackContainer
 import spock.lang.Shared
 import spock.lang.Specification
 
+import static com.github.tomakehurst.wiremock.http.Response.response
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*
 import static org.camunda.spin.Spin.S
 
 @Deployment(resources = ['./models/bpmn/send-notifications.bpmn'])
 class SendNotificationBpmnSpec extends Specification {
 
+
+    def static wmPort = 8000
+
     @ClassRule
     @Shared
     ProcessEngineRule engineRule = new ProcessEngineRule()
 
+    @ClassRule
+    @Shared
+    WireMockRule wireMockRule = new WireMockRule(wmPort)
+
+    public wireMockStub = new WireMockGroovy(wmPort)
+
 
     @Shared
     static LocalStackContainer localstack =
-            new LocalStackContainer().withServices(LocalStackContainer.Service.SES, LocalStackContainer.Service.SNS)
+            new LocalStackContainer().withServices(LocalStackContainer.Service.SES,
+                    LocalStackContainer.Service.S3,
+                    LocalStackContainer.Service.SNS)
 
 
 
@@ -49,6 +66,8 @@ class SendNotificationBpmnSpec extends Specification {
     AmazonSNSClient client
 
     def setupSpec() {
+        System.setProperty("aws.s3.formData", "formdata")
+        System.setProperty("aws.s3.pdfs", "pdfs")
         System.setProperty("ses.from.address", "from@from.com")
         localstack.start()
 
@@ -247,6 +266,99 @@ class SendNotificationBpmnSpec extends Specification {
 
         then: 'user support task should be created'
         Assert.assertThat(taskQuery().processInstanceId(instance.id).count(), Matchers.is(2L))
+    }
+
+    def 'send notification with s3 attachments'() {
+        given: 'pdf is stored in s3'
+        amazonS3.createBucket("pdfs")
+        amazonS3.putObject(new PutObjectRequest("pdfs", "testPdf.pdf",
+                new ClassPathResource("testPdf.pdf").getInputStream(), new ObjectMetadata()))
+
+        and: 'a notification payload for email'
+        def notificationPayload = S('''
+                                            {
+                                             "businessKey" : "businessKey",
+                                              "email" : {
+                                                "attachmentUrls": [
+                                                    "testPdf.pdf"
+                                                ],
+                                                "recipients" : [
+                                                  "test@test.com",
+                                                  "appples@test.com"
+                                                ]
+                                              }
+                                            }''', DataFormats.JSON_DATAFORMAT_NAME)
+
+
+        when: 'a request to send email is submitted'
+        ProcessInstance instance = runtimeService()
+                .createProcessInstanceByKey('send-notification')
+                .businessKey('businessKey')
+                .setVariables(['notificationPayload' : notificationPayload, 'initiatedBy': 'test@test.com']).execute()
+
+
+        then: 'process instance should be active'
+        assertThat(instance).isActive()
+        assertThat(instance).isWaitingAt('start')
+
+        when: 'send is executed'
+        execute(job())
+
+        then: 'email is sent'
+        Assert.assertThat(taskQuery().processInstanceId(instance.id).list().size(), Matchers.is(0))
+        assertThat(instance).hasPassed('sendSES')
+
+    }
+
+    def 'send notification with url attachments'() {
+        given: 'an endpoint that serves file'
+
+        wireMockStub.stub {
+            request {
+                method 'GET'
+                url '/files/testPdf.pdf'
+            }
+            response {
+                status: 200
+                headers {
+                    "Content-Type" "application/pdf"
+                }
+            }
+        }
+
+        and: 'a notification payload for email'
+        def notificationPayload = S('''
+                                            {
+                                             "businessKey" : "businessKey",
+                                              "email" : {
+                                                "attachmentUrls": [
+                                                    "http://localhost:8000/files/testPdf.pdf"
+                                                ],
+                                                "recipients" : [
+                                                  "test@test.com",
+                                                  "appples@test.com"
+                                                ]
+                                              }
+                                            }''', DataFormats.JSON_DATAFORMAT_NAME)
+
+
+        when: 'a request to send email is submitted'
+        ProcessInstance instance = runtimeService()
+                .createProcessInstanceByKey('send-notification')
+                .businessKey('businessKey')
+                .setVariables(['notificationPayload' : notificationPayload, 'initiatedBy': 'test@test.com']).execute()
+
+
+        then: 'process instance should be active'
+        assertThat(instance).isActive()
+        assertThat(instance).isWaitingAt('start')
+
+        when: 'send is executed'
+        execute(job())
+
+        then: 'email is sent'
+        Assert.assertThat(taskQuery().processInstanceId(instance.id).list().size(), Matchers.is(0))
+        assertThat(instance).hasPassed('sendSES')
     }
 
 }
